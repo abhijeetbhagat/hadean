40754bbbd3c2bd4a305ded482b1d523c81563d25
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-23 15:51:35 +0530

    Packetize RTP data

--- lib/connection/connection.ex
+++ lib/connection/connection.ex
@@ -1,3 +1,5 @@
+use Bitwise
+
 defmodule Hadean.RTSPConnection do
   def connect(url) do
     socket =
@@ -27,23 +29,100 @@ defmodule Hadean.RTSPConnection do
       "DESCRIBE rtsp://wowzaec2demo.streamlock.net:554/vod/mp4:BigBuckBunny_115k.mov RTSP/1.0\r\nCSeq: 3\r\nAccept: application/sdp\r\n\r\n"
     )
 
-    case :gen_tcp.recv(socket, 0) do
-      {:ok, bytes} -> bytes
-      {:error, reason} -> raise reason
+    response =
+      case :gen_tcp.recv(socket, 0) do
+        {:ok, bytes} -> bytes
+        {:error, reason} -> raise reason
+      end
+
+    session = parse_sdp(response)
+    session
+  end
+
+  # TODO abhi: figure out a way to use regex
+
+  def send(socket, :play, session) do
+    :gen_tcp.send(
+      socket,
+      "PLAY rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov/trackID=2 RTSP/1.0\r\nCSeq: 5\r\nUser-Agent: hadean\r\nSession: #{
+        session
+      }\r\nRange: npt=0.000-\r\n\r\n"
+    )
+
+    start(socket)
+  end
+
+  def send(socket, :setup, session) do
+    :gen_tcp.send(
+      socket,
+      "SETUP rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov/trackID=2 RTSP/1.0\r\nCSeq: 4\r\nUser-Agent: hadean\r\nTransport: RTP/AVP;unicast;interleaved=0-1\r\nSession: #{
+        session
+      }\r\n\r\n"
+    )
+
+    response = :gen_tcp.recv(socket, 0)
+    response
+  end
+
+  def start(socket) do
+    {:ok,
+     <<
+       packet_header::integer-8,
+       _::integer-8,
+       len::integer-16
+     >>} = :gen_tcp.recv(socket, 4)
+
+    # if packet_header is '$', then it is start of the rtp data
+    if packet_header == 0x24 do
+      {:ok, rtp_data} = :gen_tcp.recv(socket, len)
+      IO.puts(inspect(parse_packet(rtp_data)))
     end
+
+    start(socket)
   end
 
-  def send(socket, :setup) do
-    :gen_tcp.send(
-      socket,
-      "SETUP rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov RTSP/1.0\r\nCSeq: 4\r\nUser-Agent: insight\r\nTransport: RTP/AVP;unicast;interleaved=0-1\r\n\r\n"
-    )
+  @spec parse_sdp(binary) :: any
+  def parse_sdp(response) do
+    String.split(response, "\r\n")
+    |> Enum.find(fn line -> String.starts_with?(line, "Session") end)
+    |> String.split(";")
+    |> Enum.at(0)
+    |> String.split(" ")
+    |> Enum.at(1)
   end
 
-  def send(socket, :play) do
-    :gen_tcp.send(
-      socket,
-      "PLAY rtsp://184.72.239.149:554/vod/mp4:BigBuckBunny_175k.mov/trackID=2 RTSP/1.0\r\nCSeq: 5\r\nUser-Agent: insight\r\nSession: {}\r\nRange: npt=0.000-\r\n\r\n"
-    )
+  def parse_packet(rtp_data) do
+    <<
+      packet_header::integer-8,
+      marker_payload_type::integer-8,
+      seq_num::integer-16,
+      timestamp::integer-32,
+      ssrc::integer-32,
+      _rest::binary
+    >> = rtp_data
+
+    version =
+      case packet_header &&& 0x80 do
+        0 -> 1
+        _ -> 2
+      end
+
+    padding = (packet_header &&& 0x20) > 0
+    extension = (packet_header &&& 0x10) > 0
+    cc = packet_header &&& 0xF
+    marker = (marker_payload_type &&& 0x80) > 0
+    payload_type = marker_payload_type &&& 0x7F
+
+    %Hadean.Packet.RTPPacket{
+      version: version,
+      padding: padding,
+      extension: extension,
+      cc: cc,
+      marker: marker,
+      payload_type: payload_type,
+      seq_num: seq_num,
+      timestamp: timestamp,
+      ssrc: ssrc
+    }
   end
 end
--- lib/hadean.ex
+++ lib/hadean.ex
@@ -4,7 +4,9 @@ defmodule Hadean.RTSPStreamer do
   def main(_args) do
     socket = RTSPConnection.connect({3, 84, 6, 190})
     RTSPConnection.send(socket, :options)
-    sdp = RTSPConnection.send(socket, :describe)
-    IO.puts(inspect(sdp))
+    session = RTSPConnection.send(socket, :describe)
+    _response = RTSPConnection.send(socket, :setup, session)
+    response = RTSPConnection.send(socket, :play, session)
+    IO.puts(inspect(response))
   end
 end
