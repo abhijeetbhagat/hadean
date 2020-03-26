8cddeb2e4b49e5761c34a2183524a6ddb6bb3ea5
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-26 21:30:33 +0530

    Parse SDP into a ConnectionContext

--- lib/parsers/sdp-parser.ex
+++ lib/parsers/sdp-parser.ex
@@ -1,7 +1,92 @@
 defmodule Hadean.Parsers.SDPParser do
+  @spec parse_sdp(binary()) :: Hadean.Connection.ConnectionContext
   def parse_sdp(data) do
-    String.split(data, "\r\n")
-    |> Enum.find(fn line -> String.starts_with?(line, "Session") end)
+    loop(
+      String.split(data, "\r\n")
+      |> Enum.filter(fn line -> line != "" end),
+      %Hadean.Connection.ConnectionContext{}
+    )
+  end
+
+  defp loop([line | rest], state) do
+    # IO.puts("line - #{line}")
+
+    {state, rest} =
+      case binary_part(line, 0, 3) do
+        "m=a" ->
+          {audio_track, rest} = parse_audio_description(line, rest)
+          {state |> Map.put(:audio_track, audio_track), rest}
+
+        "m=v" ->
+          {video_track, rest} = parse_video_description(line, rest)
+          {state |> Map.put(:video_track, video_track), rest}
+
+        "Ses" ->
+          {state |> Map.put(:session, parse_session(line)), rest}
+
+        _ ->
+          {state, rest}
+      end
+
+    loop(rest, state)
+  end
+
+  defp loop([], state) do
+    state
+  end
+
+  defp parse_audio_description(line, lines) do
+    [_, _, _, type_num] =
+      line
+      |> String.split("=")
+      |> Enum.at(1)
+      |> String.split(" ")
+
+    audio_lines = Enum.take_while(lines, fn line -> String.starts_with?(line, "a=") end)
+
+    track =
+      common_parsing_loop(audio_lines, %Hadean.Connection.Track{type: :audio, rtp_type: type_num})
+
+    {track, lines |> Enum.drop(length(audio_lines))}
+  end
+
+  defp parse_video_description(line, lines) do
+    [_, _, _, type_num] =
+      line
+      |> String.split("=")
+      |> Enum.at(1)
+      |> String.split(" ")
+
+    audio_lines = Enum.take_while(lines, fn line -> String.starts_with?(line, "a=") end)
+
+    track =
+      common_parsing_loop(audio_lines, %Hadean.Connection.Track{type: :video, rtp_type: type_num})
+
+    {track, lines |> Enum.drop(length(audio_lines))}
+  end
+
+  defp common_parsing_loop([line | lines], track) do
+    track =
+      case binary_part(line, 0, 4) do
+        "a=fm" ->
+          track |> Map.put(:fmtp, line)
+
+        "a=co" ->
+          track |> Map.put(:id, String.split(line, ":") |> Enum.at(1))
+
+        _ ->
+          track
+      end
+
+    common_parsing_loop(lines, track)
+  end
+
+  defp common_parsing_loop([], track) do
+    track
+  end
+
+  defp parse_session(line) do
+    line
     |> String.split(";")
     |> Enum.at(0)
     |> String.split(" ")
