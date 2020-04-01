50b63518faf5afdd0591e2c8a9b432cb18297e78
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-04-01 13:38:42 +0530

    Remove trackid from play url

--- lib/commands/play.ex
+++ lib/commands/play.ex
@@ -1,13 +1,11 @@
 defmodule Hadean.Commands.Play do
   def create(url, cseq_num, session) do
-    "PLAY #{url}/trackID=2 RTSP/1.0\r\nCSeq: #{cseq_num}\r\nUser-Agent: hadean\r\nSession: #{
-      session
-    }\r\nRange: npt=0.000-\r\n\r\n"
+    "PLAY #{url} RTSP/1.0\r\nCSeq: #{cseq_num}\r\nUser-Agent: hadean\r\nSession: #{session}\r\nRange: npt=0.000-\r\n\r\n"
   end
 
   def create(url, cseq_num, session, auth) do
-    "PLAY #{url}/trackID=2 RTSP/1.0\r\nCSeq: #{cseq_num}\r\nUser-Agent: hadean\r\nSession: #{
-      session
-    }\r\nRange: npt=0.000-\r\n#{auth}\r\n\r\n"
+    "PLAY #{url} RTSP/1.0\r\nCSeq: #{cseq_num}\r\nUser-Agent: hadean\r\nSession: #{session}\r\nRange: npt=0.000-\r\n#{
+      auth
+    }\r\n\r\n"
   end
 end
